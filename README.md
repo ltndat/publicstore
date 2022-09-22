04819429ca7df0e52f417544fe4cd067eb27ba97
ltndat <ltndat@outlook.com>
2022-09-22 17:08:00 +0700

    09/22/2022 17:08:00

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ This is my store for everyone.
 
 ## Usage
 
+Open powershell as administrator and run.
+
 ```ps1
-curl.exe -fsSL https://raw.githubusercontent.com/ltndat/publicstore/main/windows.ps1 > $env:TMP/setup.ps1; pwsh $env:TMP/setup.ps1; Remove-Item $env:TMP/setup.ps1
+curl.exe -fsSL https://raw.githubusercontent.com/ltndat/publicstore/main/windows.ps1 > $env:TMP/setup.ps1; powershell $env:TMP/setup.ps1; Remove-Item $env:TMP/setup.ps1
 ```
 
 > Contains all files for flows
--- windows.ps1
+++ windows.ps1
@@ -1,1 +1,6 @@
-powershell -c "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;irm https://community.chocolatey.org/install.ps1 | iex; foreach ($i in $(curl.exe -fsSL https://raw.githubusercontent.com/ltndat/publicstore/main/choco_listapps.txt)) {choco install $i -y};"
+#Requires -RunAsAdministrator
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+irm https://community.chocolatey.org/install.ps1 | iex
+foreach ($i in $(curl.exe -fsSL https://raw.githubusercontent.com/ltndat/publicstore/main/choco_listapps.txt)) {choco install $i -y}
